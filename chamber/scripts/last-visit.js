089f0d4286528920e45949f4311149b51ced3890
ing-armandoramos <69744215+ing-armandoramos@users.noreply.github.com>
2025-06-10 11:45:52 -0700

    Changed messages

--- chamber/scripts/last-visit.js
+++ chamber/scripts/last-visit.js
@@ -1,27 +1,17 @@
-// localStorage.setItem("numVisits-ls",0);// reset the coount to 0
-/*display element variabke*/
+
 const msToDays = 86400000;
 const visits = document.querySelector("#visits");
 const h1 = document.querySelector("#visits h1");
 const p = document.querySelector("#visits p");
 
-
-//calculate future days for testing purposes
-//  const current = Date.now();
-//  const futureDays = 17;//change the number of days into the future
-//  const futureTime = current + (futureDays * msToDays);
-
-/*This gets the stored value of the numVisits-ls KEY. If it doesn't exist, then numVisits variable is given 0 as value.*/
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
-/*determine if this is the first visit*/
 if(numVisits !== 0){
     localStorage.setItem("now", Date.now());
     const now = localStorage.getItem("now");
     const firstVisit = localStorage.getItem("firstVisit");
     let numberOfDays;
 
-     //numberOfDays = (futureTime - firstVisit)/msToDays;//to simulate access to page at a future day
     numberOfDays = (now - firstVisit)/msToDays;
     modalVisit(numberOfDays, numVisits); 
 }
@@ -32,10 +22,8 @@ else{
     
 }
 
-/*Increment the number of visits by one.*/
 numVisits++;
 
-/*Store the no of visits total in local storage*/
 localStorage.setItem("numVisits-ls", numVisits);
 
 function modalVisit(days, numVisits){
@@ -45,8 +33,8 @@ function modalVisit(days, numVisits){
         p.innerHTML = `Let us know if you have any questions.`;
     }
     else if(parseInt(days) < 1){
-        h1.innerHTML = `Back so soon!  Awesome!`;
-        p.innerHTML = `We hope you enjoy your visit. `;
+        h1.innerHTML = `Thank you for coming back!`;
+        p.innerHTML = `Enjoy your visit. `;
     }else if(parseInt(days) == 1){
             h1.innerHTML = `Welcome back!`;
             p.innerHTML = `You last visited ${parseInt(days)} day ago.`;
@@ -57,5 +45,4 @@ function modalVisit(days, numVisits){
             p.innerHTML = `You last visited ${parseInt(days)} days ago.`;
         }
     
-    // visits.showModal();
 };
